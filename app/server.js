49108b590d0a6e09c923c3592acc70a3bb17101e
erickand16 <cerickandradebank@gmail.com>
2024-07-05 19:57:05 -0300

    firts

---
+++ app/server.js
@@ -0,0 +1,38 @@
+const pg = require('pg');
+
+const { Client } = pg;
+const client = new Client({
+    user: 'postgres',
+    password: '123',
+    host: 'localhost',
+    port: 5432,
+    database: 'totem',
+});
+client.connect();
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+app.post(('/salvar/:tipo/:id'), async (req, res) => {
+    var id = req.params.id;
+    let query;
+    if (req.params.tipo == "bom") {
+        query = "UPDATE avaliacao SET bom = bom + 1 WHERE id = $1";
+    }
+    if (req.params.tipo == "regular") {
+        query = "UPDATE avaliacao SET regular = regular + 1 WHERE id = $1";
+    }
+    if (req.params.tipo == "otimo") {
+        query = "UPDATE avaliacao SET otimo = otimo + 1 WHERE id = $1";
+    }
+    await client.query(query, [id]).then(() => console.log("rodou")).catch((e) => console.log(e));
+    res.status(201);
+    res.send("Deu bom");
+});
+
+app.listen(8080, () => {
+    console.log("Rodando o servidor")
+});
